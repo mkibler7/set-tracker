dd1bcc480d29a68e4a908ad385e22f41ccafea48
Michael Kibler <michael.kibler@snhu.edu>
2025-12-18 12:04:12 -0800

    feat(api): add exercises model and routes

--- backend/src/models/Exercise.ts
+++ backend/src/models/Exercise.ts
@@ -4,43 +4,40 @@ import {
   type MuscleGroup,
 } from "@reptracker/shared/muscles";
 
-const ExerciseSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true,
-      minLength: 3,
-      maxLength: 80,
-    },
-    primaryMuscleGroup: {
-      type: String,
-      required: true,
-      trim: true,
-      enum: ALL_MUSCLE_GROUPS,
-    },
-    secondaryMuscleGroups: {
-      type: [String],
-      default: [],
-      validate: {
-        validator: (arr: unknown) => {
-          if (!Array.isArray(arr)) return false;
-          // all valid muscle groups, no duplicates, and not the same as primary handled below
-          const allValid = arr.every((string) =>
-            ALL_MUSCLE_GROUPS.includes(string as MuscleGroup)
-          );
-          const uniqueCount = new Set(arr).size === arr.length;
-          return allValid && uniqueCount;
-        },
-        message:
-          "secondaryMuscleGroups must be unique and contain only allowed muscle groups",
-      },
-    },
-    description: { type: String, trim: true, maxlength: 2000 },
+const ExerciseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    minLength: 3,
+    maxLength: 80,
   },
-  { timestamps: true }
-);
+  primaryMuscleGroup: {
+    type: String,
+    required: true,
+    trim: true,
+    enum: ALL_MUSCLE_GROUPS,
+  },
+  secondaryMuscleGroups: {
+    type: [String],
+    default: [],
+    validate: {
+      validator: (arr: unknown) => {
+        if (!Array.isArray(arr)) return false;
+        // all valid muscle groups, no duplicates, and not the same as primary handled below
+        const allValid = arr.every((string) =>
+          ALL_MUSCLE_GROUPS.includes(string as MuscleGroup)
+        );
+        const uniqueCount = new Set(arr).size === arr.length;
+        return allValid && uniqueCount;
+      },
+      message:
+        "secondaryMuscleGroups must be unique and contain only allowed muscle groups",
+    },
+  },
+  description: { type: String, trim: true, maxlength: 2000 },
+});
 
 type ExerciseDoc = InferSchemaType<typeof ExerciseSchema>;
 
